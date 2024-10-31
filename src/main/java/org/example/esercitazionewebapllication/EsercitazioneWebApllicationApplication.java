9d4ca1beb852a0fdb4cec0c4274bc5fe9c1ced29
FraFalbo <falbofrancescopio@gmail.com>
2024-10-31 09:16:04 +0100

    Initial commit

---
+++ src/main/java/org/example/esercitazionewebapllication/EsercitazioneWebApllicationApplication.java
@@ -0,0 +1,13 @@
+package org.example.esercitazionewebapllication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercitazioneWebApllicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsercitazioneWebApllicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/esercitazionewebapllication/EsercitazioneWebApllicationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.esercitazionewebapllication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercitazioneWebApllicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
